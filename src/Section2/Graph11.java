151823798f5fe4b594c711d860d219a7558fe9f4
kimjeonghui <59547003+kimjeonghui@users.noreply.github.com>
2022-09-27 21:10:52 +0900

    인접 행렬 길찾기

--- src/Section2/Graph11.java
+++ src/Section2/Graph11.java
@@ -5,6 +5,33 @@ import java.util.Queue;
 
 public class Graph11 {
     public boolean getDirections(int[][] matrix, int from, int to) {
+
+        //그래프 재귀함수로 길찾기
+        //인접행렬 생성
+
+        int[][] m2 = new int[matrix.length][matrix.length];
+        //배열 초기화
+        for(int i = 0; i<matrix.length; i++){
+            for(int j=0; j<matrix.length; j++){
+                m2[i][j] = matrix[i][j];
+            }
+        }
+
+        for(int i =0; i<matrix.length; i++){
+            if(from == to) return true;
+            for(int j=0; j<matrix.length; j++){
+                if(m2[from][i]==1){
+                    m2[from][i] =0; //방문 표시
+                    if(getDirections(m2, i, to)) return true;
+                }
+            }
+        }
+        return false;
+
+
+        /*
+        큐로 구현
+
         Queue<Integer> queue = new LinkedList<>(); //너비 우선 탐색
         boolean[] visited = new boolean[matrix.length];//확인했던 노드인 걸 알기 위한 배열
         //배열초기화
@@ -32,6 +59,8 @@ public class Graph11 {
         }
 
         return false;
+        */
+
 
 
     }
