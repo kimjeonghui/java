ce3d5471f15c2c8ce353c98f05087b7bb3dda201
kimjeonghui <59547003+kimjeonghui@users.noreply.github.com>
2022-09-29 22:35:35 +0900

    알고리즘 8

---
+++ src/Section2/Solution/GCD_divideChocolateStick.java
@@ -0,0 +1,20 @@
+package Section2.Solution;
+
+import java.util.ArrayList;
+
+public class GCD_divideChocolateStick {
+    public ArrayList<Integer[]> divideChocolateStick(int M, int N) {
+        //공약수
+        //최대 M까지만 확인하면 됨
+        ArrayList<Integer[]> out= new ArrayList<>();
+
+        for(int i=1; i<=M; i++){
+            if(M%i==0 && N%i==0){
+                Integer[] list = new Integer[]{i, M/i, N/i};
+                out.add(list);
+            }
+        }
+        return out;
+
+    }
+}
