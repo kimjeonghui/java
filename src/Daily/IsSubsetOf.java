6948b83194364bdbe3531da82ba87167c5e1a94b
kimjeonghui <59547003+kimjeonghui@users.noreply.github.com>
2022-10-26 23:34:23 +0900

    데일리 코딩 24 부분집합 여

---
+++ src/Daily/IsSubsetOf.java
@@ -0,0 +1,29 @@
+package Daily;
+
+import java.util.HashSet;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class IsSubsetOf {
+    public boolean isSubsetOf(int[] base, int[] sample) {
+        HashSet<Integer> b = new HashSet();
+        HashSet<Integer> s = new HashSet();
+
+        int lengthS = sample.length;
+        int lengthB = base.length;
+
+        for(int i =0; i<lengthS; i++){
+                s.add(sample[i]);
+        }
+        for(int i =0; i<lengthB; i++){
+                b.add(base[i]);
+        }
+
+        if(b.containsAll(s)) return true;
+        else return false;
+
+    }
+
+}
+
+
