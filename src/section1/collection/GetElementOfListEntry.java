36e690719e567dd8802575c298d856f3428b17bf
kimjeonghui <20190896@sungshin.ac.kr>
2022-12-31 13:43:52 +0900

    collection 사용 복습

---
+++ src/section1/collection/GetElementOfListEntry.java
@@ -0,0 +1,17 @@
+package section1.collection;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class GetElementOfListEntry {
+    public String getElementOfListEntry(HashMap<String, List<String>> hashMap, String key, int index) {
+        if (!hashMap.containsKey(key)) {
+            return null;
+        }
+        List<String> valueList = hashMap.get(key);
+        if (index < 0 || index >= valueList.size()) {
+            return null;
+        }
+        return valueList.get(index);
+    }
+}
